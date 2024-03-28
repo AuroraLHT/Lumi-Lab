99797ff0c7edf4b1a82fd8fbd9ac21b56ac4d03f
AuroraLHT <auroralht@gmail.com>
2024-03-28 13:15:34 -0400

    pascal middleware

---
+++ pascal_server/models.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Command(BaseModel):
+    cmd : str
+
+class CommandStatus(BaseModel):
+    is_command_finished : bool
+    current_command : str
+    command_executing : str
+
+class UpdatePipelineConfig(BaseModel):
+    threshold : float
+    do_mean_clip : bool
+    do_min_max : bool
+    min_area : int
+    do_tracking : bool
+    tracker_t_min : int
+    tracker_sigma_iou : float
+
+
+class GetPipelineConfig(BaseModel):
+    threshold : float
+    do_mean_clip : bool
+    do_min_max : bool
+    classes : List[str]
+    min_area : int
+    do_tracking : bool
+    tracker_t_min : int
+    tracker_sigma_iou : float
