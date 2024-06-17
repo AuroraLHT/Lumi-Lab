8afc30e57c4a5272bec47ff5deb7f582e735a772
AuroraLHT <auroralht@gmail.com>
2024-06-17 17:27:26 -0400

    add pascal node for handling logging and command. only implemented logging

---
+++ src/Pascal/node.py
@@ -0,0 +1,78 @@
+from communication import ChamberLogMessageQueue, LiveChamberLogMessageQueue
+from log_reader import LogReader, LogReaderConfig
+
+import aio_pika
+from aio_pika import ExchangeType, connect, Message
+import asyncio
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+if __name__ == "__main__":
+
+    pass
+
+async def main(args):
+
+    # Perform connection
+    connection = await connect(f"amqp://guest:guest@{args.host}/")
+
+    # Creating a channel
+    channel = await connection.channel()
+
+    exchange_chamber = await channel.declare_exchange(
+        "chamber",
+        ExchangeType.DIRECT,
+    )
+
+    log_config = LogReaderConfig(queue_size=10, idle_time=0.5, log_path="/home/hliang16/codebase/Autonomous-Servers/pascal/data")
+
+    log_reader = LogReader(config=log_config, name="log_reader", daemon=True)
+
+    log_reader.start()
+
+    chamber_mq = ChamberLogMessageQueue(
+        log_reader=log_reader, 
+        channel=channel, 
+        exchange=exchange_chamber, 
+        routing_key="log"
+    )
+
+    live_chamber_mq = LiveChamberLogMessageQueue(
+        log_reader=log_reader, 
+        log_queue=log_reader.queue, 
+        channel=channel, 
+        exchange=exchange_chamber, 
+        routing_key=None,
+        control_routing_key="live_log_control",
+        publish_routing_key="live_log"
+    )
+
+    await chamber_mq.start()
+    await live_chamber_mq.start()
+
+    # await asyncio.sleep(3)
+    # print("send test messsage")
+    # await exchange_chamber.publish(message=Message(b''), routing_key="log")
+
+    await asyncio.Future()
+
+    await chamber_mq.cancel()
+    await live_chamber_mq.cancel()
+
+    log_reader.stop()
+    log_reader.join()
+    # await asyncio.sleep(100000)
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+                        prog='Detection Node',
+                        description='...',
+                        epilog='...')
+    parser.add_argument("--host", type=str, default="localhost")
+    args= parser.parse_args()
+
+    asyncio.run(main(args))
