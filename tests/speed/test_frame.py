432d2e1d1c4f6757e72f3caaf336db0cfe843f0a
AuroraLiang <auroralht@gmail.com>
2024-06-21 18:39:15 -0400

    add test file for speed test

---
+++ tests/speed/test_frame.py
@@ -0,0 +1,9 @@
+import tqdm
+import requests
+import json
+
+n_trials = 1000
+host = "10.229.54.118:8000"
+
+for i in tqdm.tqdm( range(n_trials), total = n_trials):
+    r = requests.get(f'http://{host}/RHEED/image')
