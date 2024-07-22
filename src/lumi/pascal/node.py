8be672a72882ed69005d782ee134804874a116e2
AuroraLHT <auroralht@gmail.com>
2024-07-22 10:55:22 -0400

    add src option to pascal node

--- src/lumi/pascal/node.py
+++ src/lumi/pascal/node.py
@@ -1,10 +1,11 @@
-from .communication import ChamberLogMessageQueueServer, LiveChamberLogMessageQueueServer
-from .log_reader import LogReader, LogReaderConfig
+from lumi.pascal.communication import ChamberLogMessageQueueServer, LiveChamberLogMessageQueueServer
+from lumi.pascal.log_reader import LogReader, LogReaderConfig, TestLogReader, TestLogReaderConfig
 
 import aio_pika
 from aio_pika import ExchangeType, connect, Message
 import asyncio
 import logging
+from pathlib import Path
 
 FORMAT = '%(asctime)s %(levelname)s:%(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
@@ -26,9 +27,13 @@ async def main(args):
         ExchangeType.DIRECT,
     )
 
-    log_config = LogReaderConfig(queue_size=10, idle_time=0.01, log_path=args.path)
 
-    log_reader = LogReader(config=log_config, name="log_reader", daemon=True)
+    if args.src == "path":
+        log_config = LogReaderConfig(queue_size=10, idle_time=0.01, log_path=args.path)
+        log_reader = LogReader(config=log_config, name="log_reader", daemon=True)
+    else:
+        log_config = TestLogReaderConfig(queue_size=10, idle_time=0.01, publish_interval=1, log_path=Path(__file__).parent / "assets/chamber_log_test.csv" )
+        log_reader = TestLogReader(config=log_config, name="test_log_reader", daemon=True)
 
     log_reader.start()
 
@@ -36,7 +41,9 @@ async def main(args):
         log_reader=log_reader, 
         channel=channel, 
         exchange=exchange_chamber, 
-        routing_key="log"
+        routing_key="log",
+        control_routing_key="log_ctrl",
+        server_name="ChamberLog"
     )
 
     live_chamber_mq = LiveChamberLogMessageQueueServer(
@@ -44,17 +51,13 @@ async def main(args):
         log_queue=log_reader.queue, 
         channel=channel, 
         exchange=exchange_chamber, 
-        routing_key=None,
-        control_routing_key="live_log_control",
-        publish_routing_key="live_log"
+        control_routing_key="live_log_ctrl",
+        publish_routing_key="live_log",
+        server_name="LiveChamberLog"
     )
 
     await chamber_mq.start()
     await live_chamber_mq.start()
-
-    # await asyncio.sleep(3)
-    # print("send test messsage")
-    # await exchange_chamber.publish(message=Message(b''), routing_key="log")
 
     await asyncio.Future()
 
@@ -63,7 +66,6 @@ async def main(args):
 
     log_reader.stop()
     log_reader.join()
-    # await asyncio.sleep(100000)
 
 if __name__ == "__main__":
     import argparse
@@ -73,6 +75,8 @@ if __name__ == "__main__":
                         description='...',
                         epilog='...')
     parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--src", type=str, default="path", help="Options [path | test]")
+
     parser.add_argument("-p", "--path", type=str, default="")
 
     args= parser.parse_args()
