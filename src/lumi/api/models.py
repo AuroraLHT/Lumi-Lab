f8f8da055429e907c4940d00b58116cd69f19c45
AuroraLHT <auroralht@gmail.com>
2024-07-25 14:13:38 -0400

    add default values

--- src/lumi/api/models.py
+++ src/lumi/api/models.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class StorageRequest(BaseModel) :
     project_name : str
-    save_ai : bool
-    save_frame : bool
-    save_log : bool
+    save_ai : bool = Field(default=False)
+    save_frame : bool = Field(default=False)
+    save_log : bool = Field(default=False)
