01ae461b972138acabea251d620d019520c694a0
AuroraLHT <auroralht@gmail.com>
2024-07-28 23:50:42 -0400

    enable high speed storage io with ThreadPoolExecution implementation.

--- src/lumi/storage/record.py
+++ src/lumi/storage/record.py
@@ -4,6 +4,13 @@ from dataclasses import dataclass
 from typing import Tuple, List, Dict
 from pathlib import Path
 import logging
+import threading
+from concurrent.futures import ThreadPoolExecutor
+import uuid
+import time
+import asyncio
+
+from typing import Union, List, Optional, Dict, Tuple, Awaitable
 
 def resize_if_over(idx, dataset, resize_step=1000, resize_absolute=None, axis=0):
     if idx >= dataset.shape[axis]:
@@ -22,7 +29,7 @@ class RecorderConfig:
     log_columns : List[str]
 
     pattern_dim : Tuple[int]
-    pattern_meta_columns : List[str] 
+    detection_meta_columns : List[str] 
 
     classifier_classes : List[str]
 
@@ -32,6 +39,11 @@ class RecorderConfig:
     save_log : bool
     save_ai : bool
 
+    force_rewrite : bool = False
+
+    compression : Optional[str | None] = None # could be gzip LZT, and 
+    compression_opts : Optional[int | None] = None  # 0-9, default value is 4
+
 class Recorder:
     NAME_LOG_DS = "log"
    
@@ -39,8 +51,6 @@ class Recorder:
     NAME_FRAME_META_DS = "frame_meta"
 
     NAME_PATTERN_DS = "pattern"
-    NAME_PATTERN_META_DS = "pattern_meta"
-
     NAME_CLASSIFICATION_DS = "classification"
     NAME_DETECTION_DS = "detection"
     NAME_INSTANCE_SEGMENTATION_DS = "instance_segmentation"
@@ -62,7 +72,8 @@ class Recorder:
 
     def open_h5(self, root_folder, project_name):
         logging.info("Try to open h5py database file at {}".format( (Path(root_folder) / f"{project_name}.h5py").absolute() ) )
-        self.h5f = h5py.File( Path(root_folder) / f"{project_name}.h5py", "a")
+        open_mode = "w" if self.config.force_rewrite else "a"
+        self.h5f = h5py.File( Path(root_folder) / f"{project_name}.h5py", open_mode)
 
     def close_h5(self):
         self.h5f.flush()
@@ -83,8 +94,11 @@ class Recorder:
                 (self.config.initial_size, img_h, img_w), 
                 maxshape=(None, img_h, img_w), 
                 chunks=True, 
-                dtype=np.uint16
+                dtype=np.uint16,
+                compression=self.config.compression,
+                compression_opts=self.config.compression_opts
             )
+            frame_dataset.attrs['size'] = 0
 
             self.frame_meta_columns = ["time_stamp", "time"]        
             frame_meta_dataset = self.h5f.create_dataset(
@@ -116,18 +130,21 @@ class Recorder:
                 self.NAME_PATTERN_DS, 
                 (self.config.initial_size, pattern_h, pattern_w), 
                 maxshape=(None, pattern_h, pattern_w), 
-                chunks=True
+                chunks=True,
+                compression=self.config.compression,
+                compression_opts=self.config.compression_opts
             )
             pattern_dataset.attrs['size'] = 0
 
-            pattern_meta_dataset = self.h5f.create_dataset(
-                self.NAME_PATTERN_META_DS, 
-                (self.config.initial_size, len(self.config.pattern_meta_columns)), 
-                maxshape=(None, len(self.config.pattern_meta_columns)), 
-                chunks=True
+            detection_meta_dataset = self.h5f.create_dataset(
+                self.NAME_DETECTION_META_DS, 
+                (self.config.initial_size, len(self.config.detection_meta_columns)), 
+                maxshape=(None, len(self.config.detection_meta_columns)), 
+                chunks=True, 
+                dtype=h5py.string_dtype(encoding='utf-8', length=None)
             )
-            pattern_meta_dataset.attrs['size'] = 0
-
+            detection_meta_dataset.attrs['size'] = 0
+            detection_meta_dataset.attrs['columns'] = self.config.detection_meta_columns
 
             classification_dataset = self.h5f.create_dataset(
                 self.NAME_CLASSIFICATION_DS, 
@@ -153,15 +170,18 @@ class Recorder:
                 (self.config.initial_size, n_init_detection, pattern_h, pattern_w), 
                 maxshape=(None, None, pattern_h, pattern_w), 
                 chunks=True, 
-                dtype=bool
+                dtype=bool, # bool is 1 bytes in C
+                compression=self.config.compression,
+                compression_opts=self.config.compression_opts
             )
             instance_segmentation_dataset.attrs['size'] = 0
 
             num_detection_dataset = self.h5f.create_dataset(
-                self.NAME_DETECTION_DS, 
+                self.NAME_NUM_DETECTION_DS, 
                 (self.config.initial_size,), 
                 maxshape=(None,), 
-                chunks=True
+                chunks=True,
+                dtype=np.int32
             )
             num_detection_dataset.attrs['size'] = 0
 
@@ -170,7 +190,8 @@ class Recorder:
                 self.NAME_NUM_TRACK_DS, 
                 (self.config.initial_size,), 
                 maxshape=(None,), 
-                chunks=True
+                chunks=True,
+                dtype=np.int32
             )
             num_tracking_dataset.attrs['size'] = 0
 
@@ -180,7 +201,8 @@ class Recorder:
                 self.NAME_TRACK_DS, 
                 (self.config.initial_size, n_init_tracking), 
                 maxshape=(None, None), 
-                chunks=True
+                chunks=True,
+                dtype=np.int32
             )
             tracking_dataset.attrs['size'] = 0
 
@@ -282,8 +304,8 @@ class Recorder:
         cls_result,
         tracking,
         detection_meta,        
-        idx,
-        resize_step
+        idx=None,
+        resize_step=None
     ):
         self.check_save(self.config.save_ai, "save_ai")
         
@@ -304,7 +326,9 @@ class Recorder:
         self.ds_pattern[_idx] = pattern
         # classification
         resize_if_over(_idx, self.ds_classification, resize_step, axis=0)
-        self.ds_classification[_idx] = cls_result.detach().cpu()[0]
+        if isinstance(cls_result, dict):
+            cls_result = [ cls_result[k] for k in self.ds_classification.attrs['class_name'] ]
+        self.ds_classification[_idx] = cls_result
         # instance segmentation
         resize_if_over(_idx, self.ds_instance_segmentation, resize_step, axis=0)
         resize_if_over(num_detection, self.ds_instance_segmentation, resize_absolute=num_detection, axis=1 )
@@ -317,8 +341,8 @@ class Recorder:
         
         self.ds_detection[_idx, :len(detections) ] = detections
 
-        resize_if_over(_idx, self.ds_frame_meta, resize_step=resize_step)
-        self.ds_detection_meta[_idx] = [ str(detection_meta[k]) for k in self.ds_frame_meta.attrs['columns'] ]
+        resize_if_over(_idx, self.ds_detection_meta, resize_step=resize_step)
+        self.ds_detection_meta[_idx] = [ str(detection_meta[k]) for k in self.ds_detection_meta.attrs['columns'] ]
 
         # tracking
         resize_if_over(_idx, self.ds_num_tracking, resize_step, axis=0)
@@ -326,7 +350,15 @@ class Recorder:
 
         resize_if_over(_idx, self.ds_tracking, resize_step, axis=0)
         resize_if_over(num_tracking, self.ds_tracking, resize_absolute=num_tracking, axis=1 )
-        self.ds_tracking[_idx, :len(tracking) ] = tracking
+        if isinstance(tracking, dict):
+            # assume tracking is region -> track relationship
+            _tracking = -np.ones(num_tracking)
+            for k, v in tracking.items():
+                _tracking[int(k)] = int(v)
+        else:
+            _tracking = tracking
+
+        self.ds_tracking[_idx, :len(tracking) ] = _tracking
 
         if idx is None: self.ds_pattern.attrs['size'] += 1
         if idx is None: self.ds_detection_meta.attrs['size'] += 1
@@ -336,6 +368,64 @@ class Recorder:
 
         if idx is None: self.ds_tracking.attrs['size'] += 1
         if idx is None: self.ds_num_tracking.attrs['size'] += 1
+
+@dataclass
+class RecorderServerConfig:
+    idle_time : 0.1
+
+class RecorderServer(threading.Thread):
+    recorder : Recorder
+    executor : ThreadPoolExecutor
+    futures : Dict[str, asyncio.Future]
+
+    def __init__(
+        self,
+        config: RecorderServerConfig,
+        recorder : Recorder,
+        name : Union[int, str]        
+    ):
+        super().__init__(name=name)
+        self.config =  config
+        self.futures = {}
+        self.executor = None
+        self.recorder = recorder
+        self._stop_event = threading.Event()
+
+    def save_prediction(self, *args, **kargs):
+        future = self.executor.submit( self.recorder.save_prediction, *args, **kargs )
+        self.futures[uuid.uuid4()] = future
+
+    def save_frame(self, *args, **kargs):
+        future = self.executor.submit( self.recorder.save_frame, *args, **kargs )
+        self.futures[uuid.uuid4()] = future
+
+    def save_log(self, *args, **kargs):
+        future = self.executor.submit( self.recorder.save_log, *args, **kargs )
+        self.futures[uuid.uuid4()] = future
+
+    def clear_futures(self):
+        self.futures = { idx : future for idx, future in self.futures.items() if not future.done() }
+
+    def run(self):
+        logging.info(f"Recorder server ({self.ident}) started")
+        self.executor = ThreadPoolExecutor(max_workers=5)
+
+        while True:
+            stop_flag = self._stop_event.wait(self.config.idle_time)
+            if stop_flag : break
+            self.clear_futures()
+
+        logging.info(f"Recorder server ({self.ident}) run loop terminated, wait for #{len(self.futures)} jobs to finish")
+        while len(self.futures):
+            # wait until all job are done
+            time.sleep(0.05)
+            self.clear_futures()
+        logging.info(f"Recorder server ({self.ident}) #{len(self.futures)} jobs left. termination complete.")
+
+    def stop(self):
+        logging.info(f"Recorder server ({self.ident}) receives a stop signal")
+        self._stop_event.set()
+
 
 
 class RecordAnalyzer:
