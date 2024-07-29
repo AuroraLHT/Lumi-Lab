26766c7dda9de2bdc149f1395980d6eb2e7b3ff0
AuroraLHT <auroralht@gmail.com>
2024-07-28 23:49:22 -0400

    refactoring test loader and log loader

--- src/lumi/pascal/log_reader.py
+++ src/lumi/pascal/log_reader.py
@@ -54,8 +54,6 @@ class LogReader(threading.Thread):
         self._io_lock = threading.Lock()
         self._reader_lock = threading.Lock()
 
-        self.create_directory_watcher()
-
     def update_log_file(self, log_file_path):
         if log_file_path != self._log_file_path:
             self._log_file_path = log_file_path
@@ -79,7 +77,7 @@ class LogReader(threading.Thread):
                 self._csv_file=None            
                 self._csv_header=None
 
-    def open_reader(self, file_name, timeout):
+    def open_reader(self, file_name, timeout, jump_to_end=True):
         logging.info(f"Open file {file_name}")
         filename = Path(file_name)
         start = time.time()
@@ -89,8 +87,9 @@ class LogReader(threading.Thread):
                 with self._reader_lock:
                     self._csv_file = filename.open(mode='r')
                     self._csv_reader = csv.DictReader(self._csv_file)
-                    self._csv_header = next(self._csv_reader, None)
-                    for row in self._csv_reader: pass # quick jump to the last row
+                    self._csv_header = self._csv_reader.fieldnames
+                    if jump_to_end:
+                        for row in self._csv_reader: pass # quick jump to the last row
                     self._csv_row_readed = 0
                     break
         
@@ -105,6 +104,7 @@ class LogReader(threading.Thread):
         """
             The main loop just keep reading row from the reader. reader would be changed if a new log file is detected.
         """
+        self.create_directory_watcher()
         self._observer.start()
         logging.info("watch dog started")
 
@@ -171,153 +171,68 @@ class LogReader(threading.Thread):
 
     @property
     def entries(self):
-        row, headers = self.get_log()
-        if row is not None:
-            return list(row.keys())
+        if self._csv_header is not None:
+            return self._csv_header
         else:
             return []
-
+        # row, headers = self.get_log()
+        # if row is not None:
+        #     return list(row.keys())
+        # else:
+        #     return []
 
 @dataclass
-class TestLogReaderConfig:
-    queue_size : int = 10
-    idle_time : float = 0.01
+class TestLogReaderConfig(LogReaderConfig):
     publish_interval : float = 1
-    log_path : str = ""
 
-class TestLogReader(threading.Thread):
+class TestLogReader(LogReader):
+    def __init__(self, config: TestLogReaderConfig, name: str, daemon: bool):
+        super().__init__(config, name, daemon)
 
-    def __init__(self, config:TestLogReaderConfig, name:str, daemon:bool):
-        super().__init__(name=name, daemon=daemon)
-
-        self._log_file_path = None
-        self._csv_file = None
-        self._csv_reader = None
-        self._csv_header = None
-
-        self.config =  config
-        self.queue = queue.Queue(maxsize=config.queue_size)
-        self.peek_queue = collections.deque(maxlen=config.queue_size) # this queue for component that need to get the latest image
-
-        self._stop_event = threading.Event()
-        self._hold_event = threading.Event()
-        self._io_lock = threading.Lock()
-        self._reader_lock = threading.Lock()
-
-        self.open_reader(self.config.log_path, 10)
-
-    def update_log_file(self, log_file_path):
-        if log_file_path != self._log_file_path:
-            self._log_file_path = log_file_path
-            self.close_reader()
-            self.open_reader(self._log_file_path, 20)
-            
-
-    def close_reader(self):
-        with self._reader_lock:
-            if self._csv_header is not None:
-                self._csv_reader= None
-                self._csv_file.close()
-                self._csv_file=None            
-                self._csv_header=None
-
-    def open_reader(self, file_name, timeout):
-        logging.info(f"Open file {file_name}")
-        filename = Path(file_name)
-        start = time.time()
-
-        while True:
-            if filename.exists():
-                with self._reader_lock:
-                    self._csv_file = filename.open(mode='r')
-                    self._csv_reader = csv.DictReader(self._csv_file)
-                    self._csv_header = next(self._csv_reader, None)
-                    self._csv_row_readed = 0
-                    break
-        
-            if time.time() - start < timeout:
-                logging.info(f"file {filename} does not exist")
-                time.sleep(0.1)
-            else:
-                raise TimeoutError(f"Cannot read the log csv file {file_name}")
-            
+    def get_content(self):
+        if self._csv_reader is not None:
+            return [ row for row in self._csv_reader ]
+        else:
+            logging.info(f"Test logger not able to get content from the csv {self._log_file_path}")
+            return []
             
     def run(self):
         """
             The main loop just keep reading row from the reader. reader would be changed if a new log file is detected.
         """
+        self.open_reader(self.config.log_path, 10, jump_to_end=False)
+        simulation_rows = self.get_content()
+
         logging.info("Test source started")
 
         while True:
-            # a= time.time()
             stop_flag = self._stop_event.wait(self.config.idle_time)
-            if stop_flag : break
+            if stop_flag : 
+                logging.info(f"Log reader thread ({self.ident}) exits the run loop")
+                break
 
-            simulation_rows = [ row for row in self._csv_reader ]
 
             hold_flag = self._hold_event.wait(self.config.idle_time)
             if hold_flag : 
-                logging.info(f"On hold, sleep for {self.config.idle_time * 10}")
+                logging.info(f"Log reader thread ({self.ident}) on hold, sleep for {self.config.idle_time * 10}")
                 time.sleep(self.config.idle_time * 10)
                 continue
-            # b= time.time()
 
-            if self._csv_reader is not None:
-                with self._reader_lock:
-                    # st = time.time()
-                    while True:
-                        # copy the dict
-                        row = dict( **simulation_rows[self._csv_row_readed % len(simulation_rows)] )
-                        row = process_row(row)
+            with self._reader_lock:
+                row = dict( **simulation_rows[self._csv_row_readed % len(simulation_rows)] )
+                row = process_row(row)
 
-                        content = (row, {})
+                content = (row, {})
 
-                        if not self.queue.full():
-                            self.queue.put(content)
-                        with self._io_lock:
-                            self.peek_queue.appendleft(content)
+                if not self.queue.full():
+                    self.queue.put(content)
+                with self._io_lock:
+                    self.peek_queue.appendleft(content)
 
-                        self._csv_row_readed += 1
-                        time.sleep(self.config.publish_interval)
-                        # et = time.time()
-                        # print("read", round(et - st, 5))
+                self._csv_row_readed += 1
+                time.sleep(self.config.publish_interval)
 
-                    # et2 = time.time()
-                    # print("total", round(et2 - st, 5))
-
-            else:
-                # await asyncio.sleep(1)
-                logging.info("empty csv_reader")
-                time.sleep(1)
-                continue
-            # c = time.time()
-            # print(b-a, c-a)
-
-    def get_log(self):
-        with self._io_lock:
-            if len(self.peek_queue):
-                # get the latest frame without popping
-                return self.peek_queue[0]
-            return (None, None)
-
-    def hold(self):
-        logging.info(f"Log reader thread ({self.ident}) receives a hold signal")
-        self._hold_event.set()
-
-    def resume(self):
-        logging.info(f"Log reader thread ({self.ident}) receives a resume signal")
-        self._hold_event.clear()
-            
     def stop(self):
         logging.info(f"Log reader thread ({self.ident}) receives a stop signal")
         self._stop_event.set()
-        self._observer.stop()
-        self._observer.join()
 
-    @property
-    def entries(self):
-        row, headers = self.get_log()
-        if row is not None:
-            return list(row.keys())
-        else:
-            return []
