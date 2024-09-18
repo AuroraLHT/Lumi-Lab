ffd26b36f7a2f2a2851e317cf81361fb25623f9b
AuroraLHT <auroralht@gmail.com>
2024-09-18 15:41:39 -0400

    add a individual test for the detection server + without the message queue, just want to how it would close it self

---
+++ tests/detection.py
@@ -0,0 +1,47 @@
+from lumi.detection.communication import (
+    DetectionMessageQueueServer,
+    LiveDetectionMessageQueueServer,
+    CameraMessageQueueClient,
+)
+from lumi.detection.model import DetectorServer, DetectorConfig
+
+import time
+import datetime
+import asyncio
+import logging
+
+import cv2
+
+import aio_pika
+from aio_pika import ExchangeType, connect
+
+FORMAT = '%(asctime)s %(levelname)s:%(message)s'
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+
+async def main(args):
+    # Perform connection
+    config = DetectorConfig(
+        input_queue_size=10,
+        output_queue_size=10,
+        model_device=None,
+        model_folder=None,
+    )
+
+    detector = DetectorServer(config=config, name="detection")
+    detector.start()
+
+    detector.join()
+    print("program exit")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+                        prog='Detection Node',
+                        description='...',
+                        epilog='...')
+    parser.add_argument("--host", type=str, default="localhost")
+    args= parser.parse_args()
+
+    asyncio.run(main(args))
