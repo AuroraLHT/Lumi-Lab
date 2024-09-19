b575e57be001ac5bc6c20a8288fd914d99b21959
AuroraLHT <auroralht@gmail.com>
2024-09-19 17:41:42 -0400

    change the run to limit the worker number

--- nodes/api.py
+++ nodes/api.py
@@ -1,10 +1,10 @@
 import uvicorn
-from lumi.api.main import app
+# from lumi.api.main import app
 
 import argparse
 
 def run(host="0.0.0.0"):
-    uvicorn.run(app, host=host, port=8000)
+    uvicorn.run("lumi.api.main:app", host=host, port=8000, workers=10)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the API server")
