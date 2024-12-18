eb73a0e45ee1a38130e75c43772a375fe16c5469
AuroraLHT <auroralht@gmail.com>
2024-12-18 17:39:44 -0500

    add a graceful timeout arg to the uvicorn server. - kill the async task when timeout - forcely solve the locking async code not shutdown issue - use with caution. need to look into the actual code when the timeout happened

--- nodes/api.py
+++ nodes/api.py
@@ -1,16 +1,27 @@
 import uvicorn
+
 # from lumi.api.main import app
 
 import argparse
 from lumi.config import settings
 
+
 def run(host):
-    uvicorn.run("lumi.api.main:app", host=host, port=settings.api.port, workers=settings.api.workers)
+    uvicorn.run(
+        "lumi.api.main:app",
+        host=host,
+        port=settings.api.port,
+        workers=settings.api.workers,
+        reload=False,
+        timeout_graceful_shutdown=30,
+    )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the API server")
-    parser.add_argument("--host", default=settings.api.host, help="Host to run the server on")
+    parser.add_argument(
+        "--host", default=settings.api.host, help="Host to run the server on"
+    )
     args = parser.parse_args()
-    
-    run(host=args.host)
 
+    run(host=args.host)
